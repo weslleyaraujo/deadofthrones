7347595ebec6c779a59cf2cc25a221adb829738b
Weslley Araujo <weslleyaraujo20@gmail.com>
2015-02-24 11:46:14 -0300

    Feature: Creating events on directive (not proud of that 😢

--- public/assets/javascripts/shared/directives/downArrow.js
+++ public/assets/javascripts/shared/directives/downArrow.js
@@ -1,17 +1,27 @@
 ;(function (module) {
   'use strict';
 
-  function DownArrow () {
+  function Directive () {
     return {
       restrict: 'E',
       templateUrl: '/partials/down-arrow.html',
+      scope: true,
       link: function ($scope, element, attr) {
+        var dom = {},
+            selector = element.find('a').attr('href');
 
-        debugger
-      }
+        dom.$all = angular.element('html, body');
+        element.find('a').on('click', function (event) {
+          dom.$all.animate({
+            scrollTop: angular.element(selector).offset().top
+          }, 400);
+
+          event.preventDefault();
+        });
+      },
     }
   }
 
-  module.directive('downArrow', DownArrow);
+  module.directive('downArrow', Directive);
 
 } (angular.module('dod.shared.directives')));
