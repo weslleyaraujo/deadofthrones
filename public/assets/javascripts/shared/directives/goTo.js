87705dfe00c26c8c48e371e2bafa84136e30d984
Mauricio Soares <m.soares2603@gmail.com>
2015-02-24 14:48:05 -0300

    create reusable directive to goTo behavior

---
+++ public/assets/javascripts/shared/directives/goTo.js
@@ -0,0 +1,19 @@
+;(function (module) {
+  'use strict';
+
+  function goTo () {
+    return {
+      restrict: 'A',
+      link: function ($scope, element, attr) {
+        element.on('click', function() {
+          angular.element('html, body').animate({
+            scrollTop: angular.element(attr.container).offset().top
+          }, 400);
+        });
+      },
+    }
+  }
+
+  module.directive('goTo', goTo);
+
+} (angular.module('dod.shared.directives')));
