bf15da90339e1cb737a7ecee43e562f48ca91d6f
Igor APA <apa.igor@gmail.com>
2015-03-02 10:50:51 -0300

    removed directory kamikaze

--- kamikaze/app.rb
+++
@@ -1,7 +0,0 @@
-require 'mongo'
-
-mongo_client = Mongo::MongoClient.new("localhost", 27017)
-db = mongo_client.db("dead_of_thrones")
-coll = db.collection("characters")
-
-puts coll.find.to_a.sample
