7de9128ed92c9ed3c194a259b5312ee332473a17
Weslley Araujo <weslleyaraujo20@gmail.com>
2015-04-26 21:26:39 -0300

    Test: Trying to use sinatra assetpack properbly as it should

--- app.rb
+++ app.rb
@@ -2,6 +2,7 @@ require 'rubygems'
 require 'sinatra'
 require 'sinatra/assetpack'
 require 'yaml'
+
 require './models/character.rb'
 
 # Set Sinatra variables
@@ -14,12 +15,26 @@ set :public_folder, 'public'
 register Sinatra::AssetPack
 assets {
 
+  serve '/assets/javascripts', from: 'public/assets/javascripts/'
+  serve '/assets/css', from: 'public/assets/css/'
+
   js :application, '/assets/javascripts/application.js', [
-    '/assets/javascripts/application.min.js'
+    '/assets/bower_components/jquery/dist/jquery.js',
+    '/assets/bower_components/angular/angular.js',
+    '/assets/javascripts/boot.js',
+    '/assets/javascripts/config.js',
+    '/assets/javascripts/shared/services/getCharacter.js',
+    '/assets/javascripts/shared/filters/clearName.js',
+    '/assets/javascripts/shared/directives/goTo.js',
+    '/assets/javascripts/shared/directives/characterImage.js',
+    '/assets/javascripts/modules/characters/characterCtrl.js'
   ]
 
   css :application, '/assets/css/application.css', [
-    '/assets/css/application.min.css'
+    '/assets/bower_components/animate.css/animate.css',
+    '/assets/fonts/armantic/stylesheet.css',
+    '/assets/fonts/deadfontwalking/stylesheet.css',
+    '/assets/css/main.css'
   ]
 }
 
